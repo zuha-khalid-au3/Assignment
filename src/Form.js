6406a0d08bfe8958f27676f1c68e3db4599fe08f
zuha-khalid-au3 <zuhakhalid@gmail.com>
2020-09-21 16:28:39 +0530

    make it better

---
+++ src/Form.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      message: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const inputValue = event.target.value;
+    const stateField = event.target.name;
+    this.setState({
+      [stateField]: inputValue,
+    });
+    console.log(this.state);
+  }
+  async handleSubmit(event) {
+    event.preventDefault();
+    const { name, message } = this.state;
+    await axios.post(
+      'https://58f6caosl8.execute-api.us-east-2.amazonaws.com/default/serverlessAppFunction',
+      { key1: `${name}, ${message}` }
+    );
+    console.log('ho gya');
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+            value={this.state.name}
+          />
+
+          <label>Message:</label>
+          <input
+            type="text"
+            name="message"
+            onChange={this.handleChange}
+            value={this.state.message}
+          />
+
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    );
+  }
+}
